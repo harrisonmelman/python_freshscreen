91992393d2982d2c6347a344e0c453c3cd67b223
anything <harrisonmelman@users.noreply.github.com>
2023-02-15 09:53:14 -0500

    edited the 17.gaj.40 example to work on mac. this is a MAC ONLY process because of a required package (fcntl) which is used by task-queue

--- 17gaj40_make_precomputed_files.bash
+++ 17gaj40_make_precomputed_files.bash
@@ -1,7 +1,15 @@
 project_code="17.gaj.40"
 label_type="WHS_heritability";
-in_dir=/u/freshscreen_n5_library/17.gaj.40;
-out_dir="/u/freshscreen_n5_library/to_S3/labels";
+is_windows=0;
+
+in_dir=/Volumes/PWP-CIVM-CTX01/freshscreen_n5_library/17.gaj.40;
+out_dir=/Volumes/PWP-CIVM-CTX01/freshscreen_n5_library/to_S3/labels;
+code_dir=/Users/harry/scratch/neuroglancer_python_prototype;
+if $is_windows; then
+    in_dir=/u/freshscreen_n5_library/17.gaj.40;
+    out_dir=/u/freshscreen_n5_library/to_S3/labels;
+    code_dir=/k/workstation/code/display/python_freshscreen/;
+fi
 
 cd ${in_dir}
 for x in BTBR C57BL_6J CAST DB2; do
@@ -10,19 +18,20 @@ for x in BTBR C57BL_6J CAST DB2; do
         # everything before the first underscore is the spec_id_fresh
         # everything after is the runno (but also trim the trailing slash)
         label_dir=${in_dir}/${x}/Done/${y}/labels*/${label_type};
-        echo label_dir $label_dir;
         spec_id_fresh=${y%_*};
         runno=${y##*_};
         runno=${runno%/*};
-        in_nii=${label_dir}/${runno}*labels*nii*;
-        in_nhdr=${in_nii%.nii*};
+        in_nii=$(ls ${label_dir}/${runno}*labels*nii);
+        in_nhdr=${in_nii%.nii};
         in_nhdr=${in_nhdr}.nhdr;
-        mk_nhdr ${in_nii};
-
-        in_file=in_nhdr;
+        if [ ! -f $in_nhdr ]; then
+            mk_nhdr ${in_nii};
+        fi
+        in_file=$in_nhdr;
         out_file=${out_dir}/${spec_id_fresh}_${runno}_${label_type}_labels.precomputed;
-        #python /k/workstation/code/display/python_freshscreen/prototype_make_precomputed_file.py ${in_file} ${out_file}
-        #exit 1;
+        if [ ! -d $out_file ]; then
+            python ${code_dir}/prototype_make_precomputed_file.py ${in_file} ${out_file} $label_type
+        fi
     done;
     cd ../../;
 done
